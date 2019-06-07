8b37854fc0fa60c06e23ec8e72bf0e0a4b8c0a8c
Bayram Bani <bayram.bani@gmail.com>
2019-06-07 14:25:57 +0100

    first commit

---
+++ doc/note.md
@@ -0,0 +1,13 @@
+# required dependency
+
+- [Parcel](https://parceljs.org/)
+
+````
+yarn global add parcel-bundler
+````
+
+- [cleandir](https://www.npmjs.com/package/clean-dir)
+
+````
+yarn global add clean-dir
+````
---
+++ src/app/page/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="row mt-5">
+        <div className="col-sm-12">
+          <h1 className="display-1 text-center">About</h1>
+        </div>
+        <div className="border">
+          <h1>test</h1>
+        </div>
+      </div>
+    </div>
+  )
+};
+export default About;
